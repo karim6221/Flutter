5f0b773691597948392688361350623f760188be
karim6221 <136124744+karim6221@users.noreply.github.com>
2024-10-07 23:24:20 +0300

    test

--- flag_quiz_app/lib/result_screen.dart
+++ flag_quiz_app/lib/result_screen.dart
@@ -6,7 +6,7 @@ class ResultScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text("data"),
+      child: Text("fbisakjfbcsakbns"),
     );
   }
 }
